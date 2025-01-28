e672fdf8c442f8a953fe1201118220b8ecdc3c30
Sadiq Haruna <sadiqharuna@Sadiqs-WorkHorse.lan>
2025-01-27 20:40:16 -0600

    Add simple login and dashboard pages

---
+++ app/dummy.tsx
@@ -0,0 +1,7 @@
+export function Dashboard() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-green-100">
+      <h1 className="text-3xl font-bold">Welcome to the Dashboard!</h1>
+    </div>
+  );
+}
